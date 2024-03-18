b03ca41680497ed3e217b0abb3bb566c26351f48
DEVang0876 <23aiml014@charusat.edu.in>
2024-03-19 00:27:53 +0530

    my func

--- teamDemo.cpp
+++ teamDemo.cpp
@@ -1,7 +1,12 @@
 //this is demo file 
 //you shoulkd code in this file 
+//everyone make their own function with their name and do something special idea 
 #include<iostream>
 using namespace std;
+void devang()
+{
+    cout<<"THIS IS DEVANG HERE";
+}
 int main()
 {
     cout<<"this is devang";
